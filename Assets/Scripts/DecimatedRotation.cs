77f0d2afe03ebdcb1ac99a9a789f1b6f410750fe
Erik Kimsey <kimseyerik@gmail.com>
2020-03-10 09:50:04 -0400

    Added rotation script to decimated triangulated torus.  Adjust colors and emission on tori

---
+++ Assets/Scripts/DecimatedRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecimatedRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 torusPosition;
+    public float rotateSpeed = 5f;
+
+    void Start()
+    {
+      Debug.Log(transform.position);
+      Debug.Log(GetComponent<Renderer>().bounds.size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+}
